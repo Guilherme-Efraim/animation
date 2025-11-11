f3fd3969fce5f5c91217d3a8250f4776de0e98b4
Guilherme-Efraim <guilhermeefraimdv@gmail.com>
2025-11-11 13:32:37 -0300

    atualizado

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const btn01 = document.getElementById("btn01")
 
+const todosBtn = [...document.querySelectorAll(".btn-icon")]
+
 btn01.addEventListener("click", () => {
-    
+    todosBtn.map((ele, posi) => {
+        let btn = ele
+        btn.classList.toggle("ativo")
+    })
 })
